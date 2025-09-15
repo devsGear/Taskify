64502b138f873de997047ad1fe8bcaaee16e64d2
devsGear <2024EB02502@online.bits-pilani.ac.in>
2025-09-15 12:54:56 +0530

    user login

--- Backend/controllers/auth.controllers.js
+++ Backend/controllers/auth.controllers.js
@@ -12,7 +12,7 @@ export async function register(req, res){
         const salt = await bcrypt.genSalt(10);
         const hashedPassword = await bcrypt.hash(password, salt);
 
-        const newUser = User.create({name, email, password: hashedPassword, createdAt: new Date()})
+        const newUser = await User.create({name, email, password: hashedPassword, createdAt: new Date()})
 
         const token = generateToken(newUser._id);
 
@@ -22,10 +22,52 @@ export async function register(req, res){
             maxAge: 15*24*60*60*1000
         });
 
-        res.sendStatus(201);
+        res.status(201).json({
+            _id: newUser._id,
+            name: newUser.name,
+            email: newUser.email
+        });
 
     } catch (error) {
         res.status(500).json({ message: "Internal server error" });
+    }
+}
+
+export async function login(req, res){
+    const {email, password} = req.body;
+
+    try {
+        if (!email || !password) {
+            return res.status(400).json({ message: "All fields Required" });
+        }
+
+        const user = await User.findOne({ email });
+
+        if (!user) {
+            return res.status(404).json({ message: "User not found" });
+        }
+
+        const passwordMatch = await bcrypt.compare(password, user.password);
+
+        if (!passwordMatch) {
+            return res.status(400).json({ message: "Password Incorret" });
+        }
+
+        const token = generateToken(user._id);
+
+        res.cookie('token', token , {
+            httpOnly:true,
+            sameSite:true,
+            maxAge: 15*24*60*60*1000
+        });
+
+        res.status(200).json({
+            name: user.name,
+            email: user.email
+        });
+        
+    } catch (error) {
+        res.status(500).json({ message: "Server Error" });
     }
 }
 
--- Backend/routes/auth.routes.js
+++ Backend/routes/auth.routes.js
@@ -1,9 +1,10 @@
 import express from "express";
 
-import { register } from "../controllers/auth.controllers.js";
+import { register, login } from "../controllers/auth.controllers.js";
 
 const authRouter = express.Router();
 
 authRouter.post('/register', register);
+authRouter.post('/login', login);
 
 export default authRouter;
