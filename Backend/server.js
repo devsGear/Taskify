e435eb8eb51e2053ab70befaad9a837b97fa4a95
devsGear <2024EB02502@online.bits-pilani.ac.in>
2025-09-16 16:18:44 +0530

    cross origin addition

--- Backend/server.js
+++ Backend/server.js
@@ -1,6 +1,7 @@
 import express from "express";
 import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
+import cors from "cors";
 
 import connectDataBase from "./config/db.js";
 import authRouter from "./routes/auth.routes.js";
@@ -11,10 +12,14 @@ dotenv.config()
 const app = express()
 const PORT = 8000
 
+connectDataBase()
+
 app.use(express.json());
 app.use(cookieParser());
-
-connectDataBase()
+app.use(cors({
+    origin: 'http://localhost:5173',
+    credentials: true, 
+}));
 
 app.use('/auth', authRouter);
 app.use('/', taskRouter)
