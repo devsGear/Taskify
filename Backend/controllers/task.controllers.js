1ceeffab5a6164c023dae7fd0d123d02d140fba0
devsGear <2024EB02502@online.bits-pilani.ac.in>
2025-09-16 16:11:38 +0530

    Delete task by id

--- Backend/controllers/task.controllers.js
+++ Backend/controllers/task.controllers.js
@@ -73,7 +73,29 @@ export async function patchIsDoneTrue(req, res){
         console.error("Error creating task:", error);
         return res.status(500).json({ message: "Server error" });
     }
-    
+}
+
+export async function deleteTaskByID(req, res){
+    try {
+        const _id = req.params.id;
+
+        const task = await Task.findById(_id);
+
+        if (!task) {
+            return res.status(404).json({ message: "Task not found" });
+        }
+
+        if (task.userId.toString() !== req.user._id.toString()) {
+            return res.status(403).json({ message: "Not authorized to update this task" });
+        }
+
+        await Task.deleteOne({ _id });
+        
+        return res.status(200).json({ message: "Task deleted successfully" });
+    } catch (error) {
+        console.error("Error deleting task:", error);
+        return res.status(500).json({ message: "Server error" });
+    }
 }
 
 
--- Backend/routes/task.routes.js
+++ Backend/routes/task.routes.js
@@ -1,5 +1,5 @@
 import express from "express";
-import { createTask, findByCategory, patchIsDoneTrue } from "../controllers/task.controllers.js";
+import { createTask, findByCategory, patchIsDoneTrue, deleteTaskByID } from "../controllers/task.controllers.js";
 import { protect } from '../middleware/auth.middleware.js';
 
 const taskRouter = express.Router();
@@ -7,5 +7,6 @@ const taskRouter = express.Router();
 taskRouter.post('/tasks', protect, createTask);
 taskRouter.get('/tasks', protect, findByCategory);
 taskRouter.patch('/tasks/:id', protect, patchIsDoneTrue);
+taskRouter.delete('/tasks/:id', protect, deleteTaskByID);
 
 export default taskRouter;
