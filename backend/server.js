f7b6b5f52f56f943c4518fd067e1c9622bcdc9a9
Ujjwal Sahu <ujjwalsahu19@gmail.com>
2026-03-13 15:45:05 +0530

    fix: cors fix

--- backend/server.js
+++ backend/server.js
@@ -16,7 +16,7 @@ connectDataBase()
 app.use(express.json());
 app.use(cookieParser());
 app.use(cors({
-    origin: 'http://localhost:5173',
+    origin: ['http://localhost:5173', 'https://taskify-mytask.vercel.app'],
     credentials: true, 
 }));
 
